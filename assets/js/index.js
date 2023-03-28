9ba25101e80c08754229a7dd5e520d7ea3bdd234
Jake Tremblay <tremblay.jake@outlook.com>
2023-03-27 18:38:32 -0700

    more updates from jake

---
+++ assets/js/index.js
@@ -0,0 +1,19 @@
+//https://medium.com/justinctollison/using-javascript-fetch-to-grab-yahoo-finance-api-949fd24876c9
+
+//const encodedParams = new URLSearchParams();
+//encodedParams.append("symbol", "GOOG");
+
+//const options = {
+//	method: 'POST',
+//	headers: {
+//		'content-type': 'application/x-www-form-urlencoded',
+//		'X-RapidAPI-Key': 'f66af50666msh7c3ae7fcc636e9dp1db22cjsn66004894965b',
+//		'X-RapidAPI-Host': 'yfinance-stock-market-data.p.rapidapi.com'
+//	},
+//	body: encodedParams
+//};
+
+//fetch('https://yfinance-stock-market-data.p.rapidapi.com/simple-info', options)
+//	.then(response => response.json())
+//	.then(response => console.log(response))
+//	.catch(err => console.error(err));
